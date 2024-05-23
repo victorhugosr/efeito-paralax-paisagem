e4cf105d4b0912670b08a7146842a2d4df3573dc
Victor Hugo <victorhugosr77@gmail.com>
2024-05-23 03:35:15 -0300

    Estrutura base quase completa

---
+++ script.js
@@ -0,0 +1,45 @@
+const titulo = document.querySelector('.titulo');
+const folha1 = document.querySelector('.folha1');
+const folha2 = document.querySelector('.folha2');
+const arbusto2 = document.querySelector('.arbusto2');
+const montanha1 = document.querySelector('.montanha1');
+const montanha2 = document.querySelector('.montanha2');
+const links = document.querySelectorAll('nav a');
+
+document.addEventListener('scroll', function() {
+    let valor = window.scrollY
+    
+    titulo.style.marginTop = valor * 1.1 + 'px'
+
+    folha1.style.marginLeft = -valor + 'px'
+    folha2.style.marginLeft = valor + 'px'
+
+    arbusto2.style.marginBottom = -valor + 'px'
+
+    montanha1.style.marginBottom = - valor * 1.1 + 'px'
+    montanha2.style.marginBottom = - valor * 1.2 + 'px'
+})
+
+links.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        /*const destinoID = this.getAttribute('href').substring(1);
+        const destinoSection = document.getElementById(destinoID);
+
+        const offset = destinoSection.getBoundingClientRect().top + window.scrollY;
+        const deslocamento = offset - document.querySelector('header').offsetHeight;
+
+        window.scrollTo({
+            top: deslocamento,
+            behavior: 'smooth'
+        });*/
+        
+        links.forEach(link => {
+            link.classList.remove('ativar');
+        });
+
+        this.classList.add('ativar');
+    });
+});
+
